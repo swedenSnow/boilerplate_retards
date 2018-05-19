8711b7865db3400550eebd374995927e2aaf65c6
Hani Abu Zeid <31956031+swedenSnow@users.noreply.github.com>
2018-05-19 20:41:22 +0200

    login bar added

--- src/App/ConfigHandler.php
+++ src/App/ConfigHandler.php
@@ -29,7 +29,7 @@ class ConfigHandler
         $config = [];
         $config['displayErrorDetails'] = true;
         $config['addContentLengthHeader'] = false;
-        $config['db']['host']   = 'localhost:3306';
+        $config['db']['host']   = 'localhost:8889';
         $config['db']['user']   = 'root';
         $config['db']['pass']   = 'root';
         $config['db']['dbname'] = 'cmsdb';
